109a591b3bc5f798f0d3b025c8f0b4f45a15348a
aboulaqi <arrachid.boulaqidate@gmail.com>
2025-01-30 18:42:27 +0100

    removed image import as it is already initialized in renderMap.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import { renderMap } from "engine/utils/renderMap.js";
 import { MAP_1,MAP_2,MAP_3,MAP_4,MAP_5 } from "game/constants/levels.js";
-import {images} from "game/constants/images.js";
 
 renderMap(MAP_1);
